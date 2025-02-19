c8fdbc8d40231acce9190e62d45007d1da620ee5
Jang <hjjang501@gmail.com>
2025-02-19 14:19:33 +0900

    Add member service component

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    // 회원가입
+    @Transactional
+    public Long create(Member member) {
+        validateDuplicateMember(member);
+        return this.memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = this.memberRepository.findByName(member.getUsername());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+
+    public List<Member> findMembers() {
+        return this.memberRepository.findAll();
+    }
+
+    public Member findOne(Long id) {
+        return this.memberRepository.findOne(id);
+
+    }
+}

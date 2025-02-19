5a7267c3fb5472f70d975dd2dc75e2d75ae94ef0
Jang <hjjang501@gmail.com>
2025-02-19 18:18:14 +0900

    Add order respository

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+    private final EntityManager em;
+
+    public Long save(Order order) {
+        em.persist(order);
+        return order.getId();
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll() {
+//        return em.createQuery("select o from Order o", Order.class)
+//                .getResultList();
+//    }
+}

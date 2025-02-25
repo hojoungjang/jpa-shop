28c4049eab3681933de93d1306fb6d23e828735e
Jang <hjjang501@gmail.com>
2025-02-25 12:12:24 +0900

    Add simple-orders v3

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -37,6 +37,12 @@ public class OrderSimpleApiController {
         return all.stream().map(order -> new SimpleOrderDto(order)).toList();
     }
 
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        return orders.stream().map(SimpleOrderDto::new).toList();
+    }
+
     @Data
     public static class SimpleOrderDto {
         private Long orderId;

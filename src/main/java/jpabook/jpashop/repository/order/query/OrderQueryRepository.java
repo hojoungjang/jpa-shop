30035b47dedda447e52a90e344148acea1b81f5d
Jang <hjjang501@gmail.com>
2025-02-26 11:59:21 +0900

    Add orders v6

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -75,4 +75,16 @@ public class OrderQueryRepository {
 
         return orderItems.stream().collect(Collectors.groupingBy(oi -> oi.getOrderId()));
     }
+
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery("select new" +
+                " jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.username, o.orderDate,\n" +
+                        "o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                " from Order o" +
+                " join o.member m" +
+                " join o.delivery d" +
+                " join o.orderItems oi" +
+                " join oi.item i", OrderFlatDto.class)
+                .getResultList();
+    }
 }
